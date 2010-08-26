9363807f52771b70731dda1a57f8bb165fbebf65
Joe Wong <lw.wang@gmail.com>
2010-08-26 01:17:15 -0400

    .

--- libs/Android.mk
+++ libs/Android.mk
@@ -3,7 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libjplayer_jni
-LOCAL_SRC_FILES := source/cc_co_androidzoo_jplayer_Player.cpp 
+LOCAL_SRC_FILES := source/cc_co_androidzoo_jplayer_Player.cpp \
+		source/player.cpp
 
 LOCAL_C_INCLUDES += \
     $(LOCAL_PATH)/include \
@@ -12,7 +13,7 @@ LOCAL_C_INCLUDES += \
     $(JNI_H_INCLUDE) 
 
 LOCAL_SHARED_LIBRARIES := \
-	libandroid_runtime libnativehelper libui libcutils libutils libbinder libsonivox libdl libCore
+	libandroid_runtime libnativehelper libui libcutils libutils libbinder libsonivox libdl
 
 LOCAL_LDLIBS += -ldl -lpthread
 
@@ -20,24 +21,3 @@ LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := libCore
-LOCAL_SRC_FILES := source/player.cpp
-
-LOCAL_C_INCLUDES += \
-    $(LOCAL_PATH)/include \
-    external/tremor/Tremor \
-    frameworks/base/core/jni \
-    $(PV_INCLUDES) \
-    $(JNI_H_INCLUDE)
-
-#LOCAL_LDLIBS := -llog
-LOCAL_SHARED_LIBRARIES := \
-        libandroid_runtime libnativehelper libui libcutils libutils libbinder libsonivox libdl 
-
-LOCAL_LDLIBS += -ldl -lpthread
-
-LOCAL_PRELINK_MODULE := false
-
-include $(BUILD_SHARED_LIBRARY)
