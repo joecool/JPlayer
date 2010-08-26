d08b2da665157308d59fc9c1e47a83e9fdec8e0d
Joe Wong <lw.wang@gmail.com>
2010-08-26 13:17:50 +0800

    .

--- src/cc/co/androidzoo/Player.java
+++ src/cc/co/androidzoo/Player.java
@@ -60,9 +60,36 @@ public class Player
     /*
      * Update the MediaPlayer ISurface. Call after updating mSurface.
      */
-    private static native final void native_init();
-    private native final void native_setup(Object mediaplayer_this);
-    private native void _setVideoSurface();
-    private native void _start();
+private native void _setVideoSurface();
+public native void setDataSource(String path) throws IOException, IllegalArgumentException, IllegalStateException;
+public native void setDataSource(FileDescriptor fd, long offset, long length)
+public native void prepare() throws IOException, IllegalStateException;
+public native void prepareAsync() throws IllegalStateException;
+private native void _start() throws IllegalStateException;
+private native void _stop() throws IllegalStateException;
+private native void _pause() throws IllegalStateException;
+private native void _flush() throws IllegalStateException;
+public native int getVideoWidth();
+public native int getVideoHeight();
+public native boolean isPlaying();
+public native void seekTo(int msec) throws IllegalStateException;
+public native int getCurrentPosition();
+public native int getDuration();
+private native void _release();
+private native void _reset();
+public native void setAudioStreamType(int streamtype);
+public native void setLooping(boolean looping);
+public native boolean isLooping();
+public native void setVolume(float leftVolume, float rightVolume);
+public native Bitmap getFrameAt(int msec) throws IllegalStateException;
+private native final int native_invoke(Parcel request, Parcel reply);
+private native final boolean native_getMetadata(boolean update_only,
+                                                    boolean apply_filter,
+                                                    Parcel reply);
+private native final int native_setMetadataFilter(Parcel request);
+private static native final void native_init();
+private native final void native_setup(Object mediaplayer_this);
+private native final void native_finalize();
+public native static int snoop(short [] outData, int kind);
 
 }
